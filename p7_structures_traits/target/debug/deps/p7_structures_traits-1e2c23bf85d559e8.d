6976b226b6a257b35c9d255d21d0d7bcf8770376
wateryfiery <dman.aman.gman@gmail.com>
2022-11-19 22:59:58 +0200

    done with structures and traits now

---
+++ p7_structures_traits/target/debug/deps/p7_structures_traits-1e2c23bf85d559e8.d
@@ -0,0 +1,5 @@
+/home/dylan/rust/tut/plural/not_rusty/p7_structures_traits/target/debug/deps/p7_structures_traits-1e2c23bf85d559e8: src/main.rs
+
+/home/dylan/rust/tut/plural/not_rusty/p7_structures_traits/target/debug/deps/p7_structures_traits-1e2c23bf85d559e8.d: src/main.rs
+
+src/main.rs:
---
+++ p7_structures_traits/target/debug/p7_structures_traits.d
@@ -0,0 +1,1 @@
+/home/dylan/rust/tut/plural/not_rusty/p7_structures_traits/target/debug/p7_structures_traits: /home/dylan/rust/tut/plural/not_rusty/p7_structures_traits/src/main.rs
